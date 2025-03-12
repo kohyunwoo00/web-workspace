e5de1cbf42733864d4b11167cbdacfc4ec917343
kohyunwoo00 <hyunwoo36003@naver.com>
2025-03-12 13:42:07 +0900

    hihi

--- MyFirstWebProject/src/main/java/com/kh/mfw/member/model/dao/MemberDAO.java
+++ MyFirstWebProject/src/main/java/com/kh/mfw/member/model/dao/MemberDAO.java
@@ -1,5 +1,15 @@
 package com.kh.mfw.member.model.dao;
 
-public class MemberDAO {
+import com.kh.mfw.member.model.dto.MemberDTO;
 
+public class MemberDAO {
+	static {
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+		}
+			
+	}
+	public void login(MemberDTO member) {
+		List<MemberDTO> list = new ArrayList();
+	}
 }
